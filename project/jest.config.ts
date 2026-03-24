91ac8650ff092030994490ff9339532c12319728
kianna-stratpoint <i_kiannaalexandra.gragg@stratpoint.com>
2026-03-24 15:52:40 +0800

    test(ui): Set up Jest and React Unit Testing library and implement Jest tests for button states, clicks, and accessibility

---
+++ project/jest.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "jest"
+import nextJest from "next/jest.js"
+
+const createJestConfig = nextJest({
+  dir: "./",
+})
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "components/**/*.{js,jsx,ts,tsx}",
+    "app/**/*.{js,jsx,ts,tsx}",
+    "lib/**/*.{js,jsx,ts,tsx}",
+    "utils/**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/*.config.ts",
+  ],
+}
+
+export default createJestConfig(config)
